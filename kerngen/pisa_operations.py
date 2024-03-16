88fd18382324da2a495bc94fa3c93a4f75110498
Hamish Hunt <hamish.hunt@intel.com>
2024-03-16 22:18:05 +0000

    WIP draft bits

--- kerngen/pisa_operations.py
+++ kerngen/pisa_operations.py
@@ -3,6 +3,37 @@
 """Module containing the low level p-isa operations"""
 
 from dataclasses import dataclass
+from typing import Generator
+
+
+# TODO Required for some of the pisa command expanding
+def batch(n: int, bsize: int = 0) -> Generator:
+    """
+    return a generator of pairs of indices that will be used typically for NTT batching
+    `n` slices will end at n and `bsize` batch size
+    """
+    UNIT_SIZE = 13
+    NUNITS = 8
+    N = 8192
+    batch_size = 8 if N <= (UNIT_SIZE << 1) else max(1, 8 // NUNITS)
+    b = batch_size if bsize == 0 else bsize
+    nq, nr = divmod(n, b)
+    us = range(nq + 1)
+    ibs = (nr if u == nq else b for u in us)
+    return ((u * b, u * b + ib) for u, ib in zip(us, ibs))
+
+
+# TODO helper object for handling polynomial expansion
+@dataclass(frozen=True)
+class Polys:
+    """helper object for handling polynomial expansion"""
+
+    symbol: str
+    number: int
+
+    def expand(self) -> str:
+        """Returns a string of the expanded symbol and ..."""
+        return ""
 
 
 @dataclass
