1375c8aa726ac05734f2146578a7f4850194b3ba
Hamish Hunt <hamish.hunt@intel.com>
2024-03-19 16:17:13 +0000

    grab context

--- kerngen/kerngen.py
+++ kerngen/kerngen.py
@@ -5,7 +5,14 @@
 """Module for generating p-isa kernels"""
 
 import sys
-from high_parser import parse_inputs
+
+from high_parser import parse_inputs, Context
+
+
+def find_context(iterable) -> Context:
+    """Return found context"""
+    g = (context for context in iterable if isinstance(context, Context))
+    return next(g)
 
 
 def main():
@@ -13,15 +20,18 @@ def main():
 
     commands = parse_inputs(sys.stdin.readlines())
 
+    # Find context should only be one at the top
+    context = find_context(commands)
+
     # string blocks of the p-isa instructions
-    pisa_ops = [
-        "\n".join(map(str, command.to_pisa()))
+    pisa_ops: list[str] = [
+        "\n".join(map(str, command.to_pisa())) if hasattr(command, "to_pisa") else None
         for command in commands
-        if hasattr(command, "to_pisa")
     ]
 
+    filtered = (t for t in zip(pisa_ops, commands) if t[0] is not None)
     hashes = "#" * 3
-    for kernel_no, (pisa_op, command) in enumerate(zip(pisa_ops, commands)):
+    for kernel_no, (pisa_op, command) in enumerate(filtered):
         print(hashes, f"Kernel ({kernel_no}):", command, hashes)
         print(pisa_op)
 
