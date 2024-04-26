d726ecb5d6e76a4100f5ea90c6de3cfb8fbc0185
Hamish Hunt <hamish.hunt@intel.com>
2024-04-26 15:13:17 +0100

    add comments

--- kerngen/pisa_generators/relin.py
+++ kerngen/pisa_generators/relin.py
@@ -23,21 +23,32 @@ class Relin(HighOp):
 
     def to_pisa(self) -> list[PIsaOp]:
         """Return the p-isa code to perform a relinearization (relin)"""
-        one = Immediate(name="one")
-        coeffs = Polys("coeff", parts=2, rns=self.input0.rns)
-        relin_key = Polys("rlk", parts=2, rns=self.input0.rns)
         # Step 1: Extend basis with special primes (ModSwitchUp)
         # Step 2: Calculate something
         # Step 3: Compute delta (rounding error correction)
         # Step 4: Compute new ctxt mod Q
         # Step 5: Add to original ctxt
 
+        one = Immediate(name="one")
+        coeffs = Polys("coeff", parts=2, rns=self.input0.rns)
+        relin_key = Polys("rlk", parts=2, rns=self.input0.rns)
+
         extend_base = ModUp(self.label, self.context, self.output, self.input0)
         # muli
         muli = Muli(self.label, self.context, self.output, self.output, one)
-
         # ntt
         ntt = NTT(self.label, self.context, coeffs, coeffs)
         # add
 
-        return [*extend_base.to_pisa(), *muli.to_pisa(), *ntt.to_pisa()]
+        return mixed_to_pisa_ops(
+            [
+                Comment("Extend base from Q to PQ"),
+                extend_base,
+                Comment("Compute something"),
+                muli,
+                Comment("Compute delta"),
+                ntt,
+                Comment("Compute new ctxt mod Q"),
+                Comment("Add to original ctxt"),
+            ]
+        )
